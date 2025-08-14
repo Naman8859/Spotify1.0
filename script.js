a75ab34d5eccd731f8df2d97c8de473bd2e84f64
Naman Singh Chauhan <namansinghchauhan6376@gmail.com>
2025-08-14 19:18:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,47 @@ document.addEventListener('click', (e) => {
         searchContainer.classList.remove('active');
     }
 });
+
+const gridItems = document.querySelectorAll(".grid1");
+
+gridItems.forEach(item => {
+    item.addEventListener("mouseenter", () => {
+        const player = item.querySelector(".player");
+        if (player) player.classList.add("activate");
+    });
+
+    item.addEventListener("mouseleave", () => {
+        const player = item.querySelector(".player");
+        if (player) player.classList.remove("activate");
+    });
+
+  item.addEventListener("click", ()=>{
+    window.location.href = "next.html";
+  })  
+});
+async function getSongs(){
+    const a = await fetch("http://127.0.0.1:5501/songs/")
+    let response = await a.text()
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
+    let song = []
+    for(let index = 0; index<as.length; index++){
+        const element = as[index];
+        if(element.href.endsWith(".mkv")){
+            song.push(element.href)
+        }
+    }
+    return song
+}
+
+async function main() {
+    let songs = await getSongs()
+    let button = document.querySelector(".grid-button")
+    button.addEventListener("click", ()=>{
+        var audio = new Audio(songs[0])
+        audio.play();
+    })
+    
+}
+main()
